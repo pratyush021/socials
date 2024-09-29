8e3aa82e40b9a53e6fc4c9246a50fa0e56207b88
Pratyush Ranjan Bohidar <53327828+pratyush021@users.noreply.github.com>
2024-09-29 08:29:49 +0530

    reset

--- README.md
+++
@@ -1,3 +0,0 @@
-command to build and run: **mvn clean install spring-boot:run**
-
-Java version 17 
--- src/app/(main)/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { Loader2 } from "lucide-react";
-
-export default function Loading() {
-  return <Loader2 className="mx-auto my-3 animate-spin" />;
-}
