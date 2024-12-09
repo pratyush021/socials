dfc6d5427a6148cd449bc9f8a219bfac7af236c8
Pratyush Ranjan Bohidar <53327828+pratyush021@users.noreply.github.com>
2024-12-09 14:49:50 +0530

    working now

--- socials/src/components/UserButton.tsx
+++ socials/src/components/UserButton.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { useSession } from "@/app/(main)/SessionProvider";
-import { DropdownMenu } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "./ui/dropdown-menu";
 import { DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 import UserAvatar from "./UserAvatar";
 import { User } from "lucia";
+import Link from "next/link";
+import { LogOutIcon, UserIcon } from "lucide-react";
+import { logout } from "@/app/(auth)/actions";
+import { cn } from "@/lib/utils";
 
 interface UserButtonProps {
   className?: string;
@@ -14,15 +24,35 @@ This is a client component so we can't call the validate session here, we have t
 */
 export default function UserButton({ className }: UserButtonProps) {
   const { user } = useSession() as { user: User };
-  console.log(user);
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className="">
+        <button className={cn("flex-none rounded-full", className)}>
           {/* <UserAvatar avatarUrl={user.avatarUrl} size={40} /> */}
-          <UserAvatar avatarUrl={null} size={40} />
+          <UserAvatar avatarUrl={user.avatarUrl} size={40} />
         </button>
       </DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Logged in as @{user.username}</DropdownMenuLabel>
+        <DropdownMenuSeparator></DropdownMenuSeparator>
+        <Link href={`/users/${user.username}`}>
+          <DropdownMenuItem>
+            <UserIcon className="mr-2 size-4" />
+            Profile
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuSeparator></DropdownMenuSeparator>
+        <DropdownMenuItem
+          onClick={() => {
+            logout();
+          }}
+        >
+          <LogOutIcon className="mr-2 size-4"></LogOutIcon>
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
